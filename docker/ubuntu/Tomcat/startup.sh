c42eca03de5ec25374d965ec67cbeb8ada9d506c
Tom Karbowski <tkarbowski@azul.com>
2020-09-21 16:24:02 -0500

    Files from tomcat-container-quickstart

--- docker/ubuntu/Tomcat/startup.sh
+++ docker/ubuntu/Tomcat/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Create a log directory for each hostname in case a shared file system is used among multiple hosts or containers.
+# As provided in the default configuration, only the access log gets written to this directory.
 LOG_DIR="${LOG_ROOT}/$(hostname)"
 mkdir -p "${LOG_DIR}"
 export JAVA_OPTS="${JAVA_OPTS} -Dlog.base=${LOG_DIR}"
