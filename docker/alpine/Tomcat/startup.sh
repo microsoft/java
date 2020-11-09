31bc1df4d4931cd76c02987000f11a6ff99d9b80
Tom Karbowski <tkarbowski@azul.com>
2020-11-09 10:34:30 -0600

    Delete startup.sh

--- docker/alpine/Tomcat/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Create a log directory for each hostname in case a shared file system is used among multiple hosts or containers.
-# As provided in the default configuration, only the access log gets written to this directory.
-LOG_DIR="${LOG_ROOT}/$(hostname)"
-mkdir -p "${LOG_DIR}"
-export JAVA_OPTS="${JAVA_OPTS} -Dlog.base=${LOG_DIR}"
-/usr/local/tomcat/bin/catalina.sh run
